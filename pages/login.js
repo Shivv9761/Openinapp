1e604bf7d319765c859bf7bb0df3f5954352e996
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-04-26 17:04:21 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -12,7 +12,7 @@ function Login() {
 
     async function handleGoogleSignin(){
         console.log('first')
-        signIn('google',{callbackUrl:"https://fastidious-pudding-b354a5.netlify.app"})
+        signIn('google',{callbackUrl:"https://dashboard-kappa-tan.vercel.app"})
     }
 
   return (
