3dd890b1ffc139da6aebad544a1fc32d94af2c1a
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-11-06 09:57:16 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,19 +51,3 @@ function UnAuthUser(){
 
 
 
-// export async function getServerSideProps({req}){
-//   const session = await getSession({req});
-
-//   if(!session){
-//     return{
-//       redirect:{
-//         destination:'/login',
-//         permanent:false
-//       }
-//     }
-//   }
-
-//   return {
-//     props:{session}
-//   }
-// }
